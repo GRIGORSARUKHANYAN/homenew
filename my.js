d00b35fc90f549560e7ccc7c454881705f511fd3
intern <saruxanyan.grigor1@mail.ru>
2022-02-17 16:50:04 +0400

    02/17/2022

--- my.js
+++ my.js
@@ -9,23 +9,17 @@
 // provideFluentDesignSystem().register(allComponents);
 
 console.log("asa");
-
-let arrfoto = [1,2,3,4,5,6,7,8,8,8,8,8]
+let arrfoto = [1,2,3,4,5,6,7,8]
 let arrtext = ["Տնօրեն","Փոխտնորեն","Քարտուղար","Մենեջեր","Ինժիներ","Հաշվապահ",
-" Տեստավորող","Օպերատոր"]
+" Տեստավորող"]
 let arrfoto_lenght=arrfoto.length
 let secret_foto = 0
-while (arrfoto_lenght>8) {
+while (arrfoto_lenght>7) {
   arrfoto_lenght=arrfoto_lenght-1
   secret_foto ++
 }
-
-
 const left = document.getElementById("leftdiv");
-
-  
-
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 7; i++) {
 left.innerHTML += `<div class="profil" >
 <img class="foto" src="img/${arrfoto[i]}.jpg" >
 <p class="p_position" >${arrtext[i]}</p>
@@ -33,8 +27,6 @@ left.innerHTML += `<div class="profil" >
 }
 
 if (secret_foto>0) {
-  
-
   left.innerHTML += `
   <div class="divaddfoto" > 
   
@@ -57,26 +49,25 @@ if (secret_foto>0) {
  const waiting1 = document.getElementById("maindiv");
 for (let i = 0; i < arrnaem_lenght; i++) {
     
-    waiting1.innerHTML +=` <div class="waiting" >
-    <div class="time" > <p>8:30</p> <p>9:00</p> </div>
-    <p class="waiting_p" > հարց N1 - զեկուցող ${arrname[i]}</p>
-    <p class="waiting_p" > Ընդունված է</p>
-    <p class="waiting_p" >    «ՀԱՅԱՍՏԱՆԻ ՀԱՆՐԱՊԵՏՈՒԹՅԱՆ ԴՐԱՄԱՆԻՇԵՐԻ ՎՃԱՐՈՒՆԱԿՈՒԹՅԱՆ,
-       ՀՆԱՄԱՇՈՒԹՅԱՆ ՀԱՏԿԱՆԻՇՆԵՐԸ: ՀԱՅԱՍՏԱՆԻ ՀԱՆՐԱՊԵՏՈՒԹՅԱՆ ՏԱՐԱԾՔՈՒՄ ԳՈՐԾՈՂ  ԲԱՆԿԵՐՈՒՄ 
-        ԿԱՆԽԻԿ ԴՐԱՄՈՎ ԿԱՏԱՐՎՈՂ ԳՈՐԾԱՌՆՈՒԹՅՈՒՆՆԵՐԸ» ԿԱՆՈՆԱԿԱՐԳ 9-Ը ՀԱՍՏԱՏԵԼՈՒ ՄԱՍԻՆ</p>
-        <fluent-divider style="margin-top: 25px";  ></fluent-divider>
-  </div> `
+    waiting1.innerHTML +=`
+    <div class="bigshablon">
+      <div class="waiting" >
+      <p class="waiting_p" > հարց N1 - զեկուցող ${arrname[i]}</p>
+      <p class="waiting_p" > Ընդունված է</p>
+      <p class="waiting_p" >    «ՀԱՅԱՍՏԱՆԻ ՀԱՆՐԱՊԵՏՈՒԹՅԱՆ ԴՐԱՄԱՆԻՇԵՐԻ ՎՃԱՐՈՒՆԱԿՈՒԹՅԱՆ,
+        ՀՆԱՄԱՇՈՒԹՅԱՆ ՀԱՏԿԱՆԻՇՆԵՐԸ: ՀԱՅԱՍՏԱՆԻ ՀԱՆՐԱՊԵՏՈՒԹՅԱՆ ՏԱՐԱԾՔՈՒՄ ԳՈՐԾՈՂ  ԲԱՆԿԵՐՈՒՄ 
+          ԿԱՆԽԻԿ ԴՐԱՄՈՎ ԿԱՏԱՐՎՈՂ ԳՈՐԾԱՌՆՈՒԹՅՈՒՆՆԵՐԸ» ԿԱՆՈՆԱԿԱՐԳ 9-Ը ՀԱՍՏԱՏԵԼՈՒ ՄԱՍԻՆ</p>
+          <div class="time" > <p>8:30</p> <p>9:00</p> </div>
+    </div>
+    </div>`
 
 
 }
 if (secret_question>0) {
-  
-
 waiting1.innerHTML += `
 <div class="divadd" > 
-
 <i  class="fa fa-chevron-circle-down" style="font-size:35px;color:red;margin: 5px; "></i>
- <H1>${secret_question}</H1> </div>`
+<H1>${secret_question}</H1> </div>`
 
 }
 
